f57f80b0d813303156024e1ed25145ba10d853ba
Tristan Price <33553882+tri-llionaire@users.noreply.github.com>
2019-01-16 13:07:22 -0500

    Update learn.py

--- learn.py
+++ learn.py
@@ -399,7 +399,7 @@ answers = {
     'Zambia':'Lusaka',
     'Zimbabwe':'Harare',
 }
-versioning = ['1.0.0', '1.0.1', '1.0.2', '1.1.0', '1.1.1', '1.1.2', '1.1.3', '1.1.4', '1.1.5', '1.1.6', '1.1.7', '1.2.0', '1.2.1', '1.2.2', '1.3.0', '1.3.1', '1.3.2', '1.4.0', '1.4.1', '1.4.2', '1.4.3', '1.5.0', '1.5.1', '1.5.2', '1.5.3', '1.5.4', '1.5.5', '1.5.6']
+versioning = ['1.0.0', '1.0.1', '1.0.2', '1.1.0', '1.1.1', '1.1.2', '1.1.3', '1.1.4', '1.1.5', '1.1.6', '1.1.7', '1.2.0', '1.2.1', '1.2.2', '1.3.0', '1.3.1', '1.3.2', '1.4.0', '1.4.1', '1.4.2', '1.4.3', '1.5.0', '1.5.1', '1.5.2', '1.5.3', '1.5.4', '1.5.5', '1.5.6', '1.5.7', '1.5.8']
 print '\nWelcome to LEARN.PY. Here you can learn the countries and capitals of the world! Type \'end\' to end the program. Let\'s get started.'
 print '\nVersion %s' % (versioning[-1])
 right = 0
@@ -420,18 +420,18 @@ if choose == '2':
 	while True:
 		n = random.randint(0, 196)
 		print '\n\n'
-		print 'What is the capital of the country \033[95m %s?\n\033[0m\n' % (get_key(answers.get(countries[n])))
+		print 'What is the capital of the country \033[95m%s?\n\033[0m\n' % (get_key(answers.get(countries[n])))
 		ans = raw_input()
 		if ans.lower() == 'end':
 			exit()
 		elif ans.lower() == answers.get(countries[n]).lower():
-			print '\n\033[92m CORRECT'
+			print '\n\033[92mCORRECT'
 			right += 1
 			total = right + wrong
 			print c_w_r(right, total)
 			print '\033[95m%s' % (str(perc(right, wrong))) + '%\033[0m\n'
 		else:
-			print '\n\033[91m WRONG. ' + answers.get(countries[n])
+			print '\n\033[91mWRONG. ' + answers.get(countries[n])
 			wrong += 1
 			total = right + wrong
 			print c_w_w(right, total)
@@ -440,18 +440,18 @@ elif choose == '1':
 	while True:
 		n = random.randint(0, 196)
 		print '\n\n'
-		print 'Which country has the capital \033[95m %s?\n\033[0m\n' % (answers.get(countries[n]))
+		print 'Which country has the capital \033[95m%s?\n\033[0m\n' % (answers.get(countries[n]))
 		ans = raw_input()
 		if ans.lower() == 'end':
 			exit()
 		elif ans.lower() == get_key(answers.get(countries[n])).lower():
-			print '\n\033[92m CORRECT'
+			print '\n\033[92mCORRECT'
 			right += 1
 			total = right + wrong
 			print c_w_r(right, total)
 			print '\033[95m%s' % (str(perc(right, wrong))) + '%\033[0m\n'
 		else:
-			print '\n\033[91m WRONG. ' + get_key(answers.get(countries[n]))
+			print '\n\033[91mWRONG. ' + get_key(answers.get(countries[n]))
 			wrong += 1
 			total = right + wrong
 			print c_w_w(right, total)
